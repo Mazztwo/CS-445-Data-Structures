21d144738573491bc3f249cd57e0a4cd336c7821
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2015-02-18 16:08:41 -0500

    Project 4

---
+++ Project 4/LinkedListPlus.java
@@ -0,0 +1,243 @@
+// Alessio Mazzone	
+// CS 445 Ramirez T,TH 230-345
+// Assignment 4
+//LinkedListPlus class. It extends A4LList and adds extra functionality with a few other methods
+
+
+public class LinkedListPlus<T> extends A4LList<T> 
+{
+	// A no arg constructor that just makes an empty list
+	public LinkedListPlus() 
+	{
+		super.clear();
+	
+	}
+	
+	// Generates a new list that is a copy of the argument list (copying all of the nodes inside the old list)
+	public LinkedListPlus(LinkedListPlus<T> oldList)
+	{
+			Node firstInOldList = oldList.firstNode;										// grabs first node in oldList
+			
+			firstNode = new Node(firstInOldList.getData())	;	   							// copies first node of old list to new first node
+			numberOfEntries++;
+			Node oldListNext = firstInOldList.getNextNode();								// grabs the second node in oldList		
+			Node newListNext = new Node(oldListNext.getData()); 			 				// grabs second node in oldList, which is equal to oldListNext
+			firstNode.setNextNode(newListNext);												// sets second node to node after first
+			oldListNext = oldListNext.getNextNode();										// grabs a third node in old list
+			numberOfEntries++;
+		
+			// This while loop will set the a new node in the newList equal to the oldListNext
+			// as long as the oldListNext node is not equal to null
+			while(oldListNext != null)
+			{												
+				newListNext.setNextNode(new Node(oldListNext.getData())); 					// the next node to the next old node
+				newListNext = newListNext.getNextNode();									// shift to next node in new list
+				oldListNext = oldListNext.getNextNode();									// shift to next node in old list
+				numberOfEntries++;
+
+			}
+			
+			
+	}
+
+	// This method will shift the list by num places to the left	
+	public void leftShift(int num)
+	{
+		if(num >= numberOfEntries )
+		{
+			super.clear();
+		}
+		else if(num <= 0)
+		{
+			// Do nothing
+		}
+		else
+		{
+			
+			for(int i = 0; i < num; i++)
+			{
+				firstNode = firstNode.getNextNode();
+				numberOfEntries--;
+			}
+			
+		}
+	}
+	
+	// This method will shift the list by num places to the right
+	public void rightShift(int num)
+	{
+		if(num >= numberOfEntries )
+		{
+			super.clear();
+		}
+		else if(num <= 0)
+		{
+			// Do nothing
+		}
+		else
+		{	
+			
+			Node newTailNode = firstNode;
+			
+			// this for makes the new tail node = to entry at numEntries - num
+			for(int i = 1; i <= (numberOfEntries - num) - 1; i ++)
+			{
+				newTailNode = newTailNode.getNextNode();
+			}
+			
+			newTailNode.setNextNode(null);   //set the node after the new tail equal to null
+			numberOfEntries -= num;
+			
+		}
+	}
+	
+	// This method will still shift the contents of the list num places to the left, 
+	// but rather than removing nodes from the list, it will change their 
+	// ordering in a cyclic way
+	public void leftRotate(int num)
+	{
+		if(num == 0)
+		{
+			// Do nothing
+		}
+		else if( num > 0 && num < numberOfEntries)
+		{
+			Node lastNode = firstNode;
+			
+			// this for loop will grab the last node in the list
+			for(int i = 1; i <= numberOfEntries - 1  ; i++)
+			{
+				lastNode = lastNode.getNextNode();
+			}
+			
+			lastNode.setNextNode(firstNode);		// This sets the last node to point to the first node, momentarily making the list circular
+			
+			Node newEnd = firstNode;
+			
+			// This for loop will grab the node at position num. This node will be made the new end of the list
+			for(int i = 0; i < num - 1; i++ )
+			{
+				newEnd = newEnd.getNextNode();
+			}
+			
+			firstNode = newEnd.getNextNode();		// Set new head to be the num + 1 node
+			newEnd.setNextNode(null);				// Set the new end's next node to be null
+			
+		}
+		else if(num > numberOfEntries)
+		{
+			while(num > numberOfEntries)
+			{
+				num -= numberOfEntries;
+			}
+			
+			leftRotate(num);
+		}
+		else if(num < 0)
+		{
+			num = num * (-1);
+			rightRotate(num);
+			
+		}
+	}
+	
+	// This method will still shift the contents of the list num places to the right, 
+	// but rather than removing nodes from the list, it will change their 
+	// ordering in a cyclic way
+	public void rightRotate(int num)
+	{
+		if(num == 0)
+		{
+			// Do nothing
+		}
+		else if( num > 0 && num < numberOfEntries)
+		{
+			Node lastNode = firstNode;
+			
+			// this for loop will grab the last node in the list
+			for(int i = 1; i <= numberOfEntries - 1  ; i++)
+			{
+				lastNode = lastNode.getNextNode();
+			}
+			
+			lastNode.setNextNode(firstNode);		// This sets the last node to point to the first node, momentarily making the list circular
+			
+			Node newEnd = firstNode;
+			
+			// This for loop will grab the node at position numberOfEntries - num - 1. This node will be made the new end
+			for(int i = 0; i < (numberOfEntries - num) - 1 ; i++ )
+			{
+				newEnd = newEnd.getNextNode();
+			}
+			
+			firstNode = newEnd.getNextNode();		// Set new head 
+			newEnd.setNextNode(null);				// Set the new end's next node to be null
+			
+			
+		}
+		else if(num > numberOfEntries)
+		{
+			while(num > numberOfEntries)
+			{
+				num -= numberOfEntries;
+			}
+			
+			rightRotate(num);
+		}
+		else if(num < 0)
+		{
+			num = num * (-1);
+			leftRotate(num);
+			
+		}
+	}
+	
+	
+	// This method will reverse the nodes in the list
+	public void reverse()
+	{
+		LinkedListPlus<T> temp = new LinkedListPlus<T>();					// a temporary list to hold the reversed list
+		
+		temp.firstNode = new Node(firstNode.getData());						// copy firstNode data to temp.firstNode
+	
+		if(firstNode.getNextNode() != null)
+		{
+		
+			Node nextOldList = firstNode.getNextNode();							// get the next node in the old list
+			Node nextNewList = new Node(nextOldList.getData());					// make a new node with the old list data in it
+		
+		
+			// this while loop will take the data in the old new list, and add it to the front of the temp list, thus reversing the list
+			while(nextOldList != null)
+			{
+				nextNewList.setNextNode(temp.firstNode);
+				temp.firstNode = nextNewList;
+				nextOldList = nextOldList.getNextNode();
+				if(nextOldList != null)
+					nextNewList = new Node(nextOldList.getData());
+		
+			}
+		
+			firstNode = temp.firstNode;										// now make the temp list the new list by making firstNode point to temp.firstNode
+		}
+		else
+			firstNode = temp.firstNode;
+			
+	}
+	
+	// Returns a string representation of the list associated with the object
+	public String toString()
+	{
+		String stringList = "";
+		Node currNode = firstNode;
+		
+		while(currNode != null)
+		{
+			stringList += currNode.getData() + " ";
+			currNode = currNode.getNextNode();
+		}
+				
+		return stringList;
+	}
+	
+}
---
+++ Project 4/ReallyLongInt.java
@@ -0,0 +1,244 @@
+// Alessio Mazzone	
+// CS 445 Ramirez T,TH 230-345
+// Assignment 4
+// ReallyLongInt class. Extenrs the LinkedListPlus and adds more methods. Also uses comparable
+
+import java.util.*;
+
+public class ReallyLongInt extends LinkedListPlus<Integer> implements Comparable<ReallyLongInt> 
+{
+
+	// Default constructor
+	public ReallyLongInt()
+	{
+		super();
+	}
+
+	// Note that we are adding the digits here in the FRONT. This is more efficient
+	// (no traversal is necessary) and results in the LEAST significant digit first
+	// in the list.
+	public ReallyLongInt(String s)
+	{
+		super();
+		char c;
+		int digit;
+		// Iterate through the String, getting each character and converting it into
+		// an int.  Then make an Integer and add at the front of the list.
+		for (int i = 0; i < s.length(); i++)
+		{
+			c = s.charAt(i);
+			if (('0' <= c) && (c <= '9'))
+			{
+				digit = c - '0';
+				this.add(1, new Integer(digit));
+			}
+		}
+	}
+	// Simple call to super to copy the nodes from the argument ReallyLongInt into a
+	// new one.
+	public ReallyLongInt(ReallyLongInt rightOp)
+	{
+		super(rightOp);
+	}
+		
+	// Method to put digits of number into a String.  Since the numbers are
+	// stored "backward" (least significant digit first) we first reverse the
+	// number, then traverse it to add the digits to a StringBuilder, then
+	// reverse it again.  This seems like a lot of work, but accessing the list
+	// from back to front using repeated calls to the getEntry() method is actually
+	// much more work -- we will discuss this in lecture after break.
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder();
+		if (numberOfEntries > 0)
+		{
+			this.reverse();
+			for (Node curr = firstNode; curr != null; curr = curr.next)
+			{
+				sb.append(curr.data);
+			}
+			this.reverse();
+		}
+		return sb.toString();
+	}
+
+	// Returns a NEW ReallyLongInt that is the sum of the current ReallyLongInt and the parameter ReallyLongInt, without altering the original values
+	public ReallyLongInt add(ReallyLongInt rightOp)
+	{
+		int carry = 0;							// Holds the carry
+		StringBuilder sum = new StringBuilder();						// Holds the sum that will be turned into a ReallyLongInt via the constructor at the end
+		int singleDigitSum = 0;					// Holds the sum of the single digits that the add is currently on
+		
+		singleDigitSum = firstNode.getData() + rightOp.firstNode.getData();
+		
+		if(singleDigitSum < 10) 
+		{
+			sum.append(Integer.toString(singleDigitSum));
+		}
+		else
+		{
+			carry = ((singleDigitSum) / (10));
+			sum.append(Integer.toString(singleDigitSum - 10));
+		}
+		
+		Node next = firstNode.getNextNode();
+		Node nextRightOp = rightOp.firstNode.getNextNode();
+		
+		while(next != null || nextRightOp != null)
+		{
+			if(next == null)
+			{
+				singleDigitSum = carry + nextRightOp.getData();
+				
+				if(singleDigitSum < 10)
+				{
+					sum.insert(0, Integer.toString(singleDigitSum));
+					nextRightOp = nextRightOp.getNextNode();
+				}
+				else
+				{
+					carry = ((singleDigitSum) / (10));
+					
+					while(singleDigitSum >= 10)
+					{
+						singleDigitSum -= 10;
+					}
+					
+					sum.insert(0, Integer.toString(singleDigitSum));
+					nextRightOp = nextRightOp.getNextNode();
+				}
+			}
+			else if (nextRightOp == null )
+			{
+				singleDigitSum = carry + next.getData();
+				
+				if(singleDigitSum < 10)
+				{
+					sum.insert(0, Integer.toString(singleDigitSum));
+					next = next.getNextNode();
+				}
+				else
+				{
+					carry = ((singleDigitSum) / (10));
+					
+					while(singleDigitSum >= 10)
+					{
+						singleDigitSum -= 10;
+					}
+					
+					sum.insert(0, Integer.toString(singleDigitSum));
+					next = next.getNextNode();
+				}
+			}
+			else
+			{
+				singleDigitSum = carry + next.getData() + nextRightOp.getData();	
+
+				if(singleDigitSum < 10)
+				{
+					sum.insert(0, Integer.toString(singleDigitSum));
+					next = next.getNextNode();
+					nextRightOp = nextRightOp.getNextNode();
+				}
+				else
+				{
+					carry = ((singleDigitSum) / (10));
+					
+					while(singleDigitSum >= 10)
+					{
+						singleDigitSum -= 10;
+					}
+					
+					sum.insert(0, Integer.toString(singleDigitSum));
+					next = next.getNextNode();
+					nextRightOp = nextRightOp.getNextNode();
+				}
+			}	
+		}
+		sum.insert(0, carry);
+		return new ReallyLongInt(sum.toString());	
+	}
+	
+	// Multiply the current ReallyLongInt by 10^num	
+	public void multTenToThe(int num)
+	{
+		if(num == 0)
+		{
+			// do nothing
+		}
+		else
+		{
+			while(num != 0)
+			{
+				
+				Node temp = new Node(new Integer(0));
+				temp.setNextNode(firstNode);
+				firstNode = temp;
+				numberOfEntries++;
+				num--;
+			}		
+		}
+	}
+
+	// Divide the current ReallyLongInt by 10^num
+	public void divTenToThe(int num)
+	{	
+		if(num >= numberOfEntries)
+		{
+			firstNode = new Node(new Integer(0));
+			numberOfEntries = 0;
+		}
+		else
+		{
+			leftShift(num);
+
+		}
+	}
+	
+	// this method checks to see if two ReallyLongInts are equal
+	public boolean equals(Object rightOp)
+		{
+			ReallyLongInt rightSide = (ReallyLongInt)rightOp;
+			
+			int comparison = this.compareTo(rightSide);
+			
+			if(comparison == 0)
+				return true;
+			else
+				return false;
+				
+		}
+	
+	// implemented to satisfy the comparable interface
+	public int compareTo(ReallyLongInt rightOp) 
+	{
+		if(numberOfEntries > rightOp.numberOfEntries)
+		{
+			return 1;
+		}
+		else if ( numberOfEntries < rightOp.numberOfEntries)
+		{
+			return -1;
+		}
+		else
+		{
+			for(int i = numberOfEntries; i > 0; i--)
+			{
+				int comparison = getEntry(i).compareTo(rightOp.getEntry(i));
+				
+				if(comparison == 1)
+				{
+					return 1;
+				}
+				else if(comparison == -1)
+				{
+					return -1;
+				}
+			}
+			
+			return 0;
+		}
+		
+	}
+
+}
