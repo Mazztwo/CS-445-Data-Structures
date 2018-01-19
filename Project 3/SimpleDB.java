9deb2ca856c01596320b819190393b69931e4f8b
Mazztwo <alessio.mazzone12345@gmail.com>
2018-01-19 12:30:00 -0500

    Update SimpleDB.java

--- Project 3/SimpleDB.java
+++ Project 3/SimpleDB.java
@@ -1,28 +1,14 @@
-// CS 0445 Spring 2014
+// SimpleDB<T> interface
 
-
-// Assignment 1 SimpleDB<T> interface
-// Carefully read the specifications for each of the operations and
-// implement them correctly in your MyDB<T> class.
 
 // Since SimpleDB<T> is an interface, there is no implicit requirement for
-// the physical storage of the actual data.  However, for Assignment 1 you
-// are required to use a Java array as your underlying data in MyDB<T>
-// class.  You MAY NOT use ArrayList, Vector or any predefined collection
-// class for your MyDB<T> data.  However, you may want to use some
-// additional instance variables to keep track of the data effectively.
+// the physical storage of the actual data.  
 
 public interface SimpleDB<T>
 {
 	// Add a new item to the SimpleDB in the next available location.  If
 	// all goes well, return true.
-	// IMPLEMENTATION REQUIREMENT:  Although the interface shown here does not
-	// require any specific implementation, for Assignment 1 you are required to use
-	// an underlying array for your data.  If the underlying array is full when you
-	// attempt an addItem(), you are required to double the size of the underlying
-	// array, and to output a message stating what the new size is.  Based on this
-	// requirement, the addItem() method should always succeed, barring some
-	// catastrophic error.
+
 	public boolean addItem(T item);
 	
 	// Find, remove and return the item matching the argument in the SimpleDB.  Use the
